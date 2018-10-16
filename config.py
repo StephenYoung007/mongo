16ea3582fb7eb103f2c458e6bab1610f8bd8ddb5
wy1309374767@gmail.com <wy1314520>
2018-10-16 21:53:46 +0800

    add login page

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ secret_key = 'stephen young'
 
 accept_user_file_type = ['img', 'png', 'jpg', 'jif']
 
-# user_file_directory = r'C:\Users\wuyan\Desktop\flask\mongo\user_img'
-# data_file_directory = r'C:\Users\wuyan\Desktop\flask\mongo\data'
+user_file_directory = r'C:\Users\wuyan\Desktop\flask\mongo\user_img'
+data_file_directory = r'C:\Users\wuyan\Desktop\flask\mongo\data'
 
 # user_file_directory = r'/var/www/mongo/user_img'
 # data_file_directory = r'/var/www/mongo/data'
 
-user_file_directory = r'E:\mongo\user_img'
-data_file_directory = r'E:\mongo\data'
+# user_file_directory = r'E:\mongo\user_img'
+# data_file_directory = r'E:\mongo\data'
